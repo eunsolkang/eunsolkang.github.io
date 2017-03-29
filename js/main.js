89e8c977f5b5564297e3d949e3bb0b3913cd6404
강은솔 <eunsol2953@naver.com>
2017-03-29 23:07:28 +0900

    dkdk

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
+var len
+$(document).ready(function(){
+  len = $('.project-pop').length;
+});
 $(document).mouseup(function (e) {
-  if($('#open').css('display') != 'none')
+  for (var i=0; i<len; i++)
   {
-    var container = $(".project-pop-outline");
-    if (!container.is(e.target) && container.has(e.target).length === 0){
-      window.location.href = "#close"
+    var text = "#open"+i;
+    if($(text).css('display') != 'none')
+    {
+      var container = $(".project-pop-outline");
+      if (!container.is(e.target) && container.has(e.target).length === 0){
+        window.location.href = "#close"
+      }
     }
   }
 
